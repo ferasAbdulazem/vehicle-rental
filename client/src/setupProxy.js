54607fc96725dc20a7c6757742ef735d9f9c60f0
Feras Abdulazem <feras.abdulazem@holzweg.com>
2022-10-23 23:07:07 +0200

    fix proxy setup

---
+++ client/src/setupProxy.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:4000',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '', // remove base path
+      },
+    }),
+  );
+};
